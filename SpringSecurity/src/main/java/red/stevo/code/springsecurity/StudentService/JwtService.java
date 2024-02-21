f824d12e7a6dafb5cc2fb7b64a7c2093b1c92ae3
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-21 09:00:58 +0300

    working on environment variables

--- SpringSecurity/src/main/java/red/stevo/code/springsecurity/StudentService/JwtService.java
+++ SpringSecurity/src/main/java/red/stevo/code/springsecurity/StudentService/JwtService.java
@@ -1,7 +1,18 @@
 package red.stevo.code.springsecurity.StudentService;
 
-public class JwtService {
 
+import org.springframework.stereotype.Component;
 
+import java.util.function.Function;
 
+@Component
+public class JwtService{
+    public String extractUserName(String jwtToken)
+    {
+        return null;
+    }
+
+    public <T, Claim> T extractClaim(String jwtToken, Function<Claim, T> getAClaim){
+        return  null;
+    }
 }
