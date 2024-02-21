8c0ae410e70bfc2929f6031b61e5275be0f999b6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-21 15:42:26 +0300

    created the user DTO and implemented the UserDetails interface

---
+++ SpringSecurity/src/main/java/red/stevo/code/springsecurity/SchoolDTO/StudentDTO.java
@@ -0,0 +1,59 @@
+package red.stevo.code.springsecurity.SchoolDTO;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "students_info")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDTO implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "user_name")
+    String userName;
+
+    @Column(name = "password")
+    String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
