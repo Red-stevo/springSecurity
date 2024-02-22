06bc30f9ad1c00231a36f29b89312b7a48fa48d1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-22 08:29:02 +0300

    working on the filter chain

--- SpringSecurity/src/main/java/red/stevo/code/springsecurity/Filters/JwtAuthenticationFilter.java
+++ SpringSecurity/src/main/java/red/stevo/code/springsecurity/Filters/JwtAuthenticationFilter.java
@@ -4,20 +4,31 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain)
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
+        /*Check if the request sent has a header or not,
+        * if the request does not have a header,
+        * we pass the request to the next filter chain*/
+        String authorizationHeader = request.getHeader("Authorization");
 
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
+            log.info("Forwarding the request to the next filter chain");
+            filterChain.doFilter(request,response);
+        }
     }
 }
