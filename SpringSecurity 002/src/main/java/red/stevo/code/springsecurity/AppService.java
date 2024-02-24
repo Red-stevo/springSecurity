f285de1d78a3ad2a99836a1b53c0b7d0476ba15d
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-24 07:08:11 +0300

    configured the UserDetailService

--- SpringSecurity 002/src/main/java/red/stevo/code/springsecurity/AppService.java
+++ SpringSecurity 002/src/main/java/red/stevo/code/springsecurity/AppService.java
@@ -1,25 +1,37 @@
 package red.stevo.code.springsecurity;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.core.userdetails.UserDetails;
+import red.stevo.code.springsecurity.AppRepository.StudentRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import red.stevo.code.springsecurity.AppRepository.StudentRepository;
 
-import java.util.concurrent.atomic.AtomicReference;
 
+@Service
+@Data
+@Slf4j
 public class AppService implements UserDetailsService {
 
     private final StudentRepository studentRepository;
 
+    @Autowired
     public AppService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.error("Getting the user by username");
         UserDetails userDetails;
        userDetails = studentRepository.findAllByUsername(username).orElseThrow(
-               () -> new UsernameNotFoundException("Username Not Found")
+               () -> {
+                   log.error("Username Not Found");
+                   return new UsernameNotFoundException("Username Not Found");
+               }
        );
 
        return userDetails;
