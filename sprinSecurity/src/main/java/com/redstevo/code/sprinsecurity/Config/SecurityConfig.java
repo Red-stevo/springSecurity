3231907d669a2c69240f4a3802b33fdc2683cc05
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-19 06:30:31 +0300

    modified the database configuration

---
+++ sprinSecurity/src/main/java/com/redstevo/code/sprinsecurity/Config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.redstevo.code.sprinsecurity.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
--- sprinSecurity/src/main/java/com/redstevo/code/sprinsecurity/Controllers/AuthenticationController.java
+++ sprinSecurity/src/main/java/com/redstevo/code/sprinsecurity/Controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.redstevo.code.sprinsecurity.Models.AuthenticationRequestModel;
 import com.redstevo.code.sprinsecurity.Models.AuthenticationResponseModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthenticationResponseModel> registerUser(
             @RequestBody AuthenticationRequestModel authenticationRequestModel
             ){
 
+        return null;
     }
 }
