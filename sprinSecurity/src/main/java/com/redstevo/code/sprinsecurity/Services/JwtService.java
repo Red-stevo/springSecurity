2f97a7015bc066ba250b8101a03218920290ae8b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-19 12:58:19 +0300

    configured the jwtService

--- sprinSecurity/src/main/java/com/redstevo/code/sprinsecurity/Services/JwtService.java
+++ sprinSecurity/src/main/java/com/redstevo/code/sprinsecurity/Services/JwtService.java
@@ -1,18 +1,16 @@
 package com.redstevo.code.sprinsecurity.Services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.hibernate.annotations.SecondaryRow;
-import org.hibernate.boot.archive.scan.internal.ScanResultImpl;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.codec.Decoder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -37,5 +35,34 @@ public class JwtService {
                 .hmacShaKeyFor(bytes);
     }
 
+    private Claims extractAllClaims(String jwt){
+        return Jwts
+                .parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+    }
+
+    private <T> T getClaim(Function<Claims, T> claimsExtractor, String jwt){
+        Claims claims = extractAllClaims(jwt);
+        return claimsExtractor.apply(claims);
+    }
+
+    public String getUsername(String jwt){
+        return getClaim(Claims::getSubject, jwt);
+    }
+
+    public Boolean isValid(UserDetails userDetails, String jwt){
+        return isExpired(jwt);
+    }
+
+    private Date getExpiration(String jwt){
+        return getClaim(Claims::getExpiration, jwt);
+    }
+
+    private Boolean isExpired(String jwt){
+        return new  Date(System.currentTimeMillis()).before(getExpiration(jwt));
+    }
 
 }
